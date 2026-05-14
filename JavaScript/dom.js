0a4d00f41e79d1b2f76fce6f319c0b5a6f7be8fa
xinxie29-wq <xinxie29@bergen.org>
2026-05-14 14:39:48 -0400

    selecting buttons
    Co-authored-by: saf-kom29 <saf-kom29@users.noreply.github.com>

---
+++ JavaScript/dom.js
@@ -0,0 +1,25 @@
+// Elements
+
+// Buttons
+const moodBtn = document.querySelectorAll(".mood-btn")
+const happyBtn = document.getElementById("happy-btn")
+const exciteBtn = document.getElementById("excited-btn")
+const calmBtn = document.getElementById("calm-btn")
+const confidentBtn = document.getElementById("confident-btn")
+const loneBtn = document.getElementById("lonely-btn")
+const sadBtn = document.getElementById("sad-btn")
+const tiredBtn = document.getElementById("tired-btn")
+const anxiousBtn = document.getElementById("anxious-btn")
+const angryBtn = document.getElementById("angry-btn")
+
+const submitBtn = document.getElementById("journal-submit-btn")
+
+const searchBtn = document.getElementById("music-search-btn")
+
+
+// Music Section
+const musicList = document.querySelector(".music-list")
+const musicCard = document.querySelector(".music-card")
+
+const musicInput = document.getElementById("music-search-input")
+
